7b34e01448a33fec244941fe1c744ebbbd2deb03
zoccheQA <zocche.carolineqa@gmail.com>
2025-03-24 17:30:05 -0300

    First commit

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,30 @@
+describe('Orange HRM Tests', () => {
+  it('Login - Success', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type ('Admin')
+    cy.get("[name='password']").type ('admin123')
+    cy.get("[type='submit']").click ()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get('.oxd-text--h6').contains('Dashboard')
+  })
+  it('Login - Fail - Incorect User name', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type ('Test')
+    cy.get("[name='password']").type ('admin123')
+    cy.get("[type='submit']").click ()
+    cy.get('.oxd-alert-content > .oxd-text')
+  })
+  it('Login - Fail - Incorect Password', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type ('Adim')
+    cy.get("[name='password']").type ('admin1234')
+    cy.get("[type='submit']").click ()
+    cy.get('.oxd-alert-content > .oxd-text')
+  })
+  it('Login - Fail - Password not Added', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type ('Admin')
+    cy.get("[type='submit']").click ()
+    cy.get('.oxd-input-group > .oxd-text')
+})
+})
