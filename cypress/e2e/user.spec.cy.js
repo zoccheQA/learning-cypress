5ed71036c96aacb9c2ec0304e600933221a6ca7a
zoccheQA <zocche.carolineqa@gmail.com>
2025-04-08 09:13:03 -0300

    user-data added

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -13,30 +13,34 @@ describe('Orange HRM Tests', () => {
  
   it('Login - Success', () => {
     cy.visit('/web/index.php/auth/login')
-    cy.get(selectorslist.usernameField).type ('Admin')
-    cy.get(selectorslist.passwordField).type ('admin123')
+    cy.get(selectorslist.usernameField).type (userData.userSuccess.username)
+    cy.get(selectorslist.passwordField).type (userData.userSuccess.password)
     cy.get(selectorslist.loginButton).click ()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorslist.dasboardGrid)
   })
   it('Login - Fail - Incorect User name', () => {
     cy.visit('/web/index.php/auth/login')
-    cy.get(selectorslist.usernameField).type ('Test')
-    cy.get(selectorslist.passwordField).type ('admin123')
+    cy.get(selectorslist.usernameField).type (userData.userFail.username)
+    cy.get(selectorslist.passwordField).type (userData.userSuccess.password)
     cy.get(selectorslist.loginButton).click ()
     cy.get(selectorslist.worngCredentialAlertInvalidCredentials)
   })
   it('Login - Fail - Incorect Password', () => {
     cy.visit('/web/index.php/auth/login')
-    cy.get(selectorslist.usernameField).type ('Adim')
-    cy.get(selectorslist.passwordField).type ('admin1234')
+    cy.get(selectorslist.usernameField).type (userData.userSuccess.username)
+    cy.get(selectorslist.passwordField).type (userData.userFail.password)
     cy.get(selectorslist.loginButton).click ()
     cy.get(selectorslist.worngCredentialAlertInvalidCredentials)
   })
   it('Login - Fail - Password not Added', () => {
     cy.visit('/web/index.php/auth/login')
-    cy.get(selectorslist.usernameField).type ('Admin')
+    cy.get(selectorslist.usernameField).type (userData.userSuccess.username)
     cy.get(selectorslist.loginButton).click ()
     cy.get(selectorslist.WorngCredentialAlertRequiredPassword)
 })
+
+
+
 })
+
