3dfd43c0fe358272805df0d5858eccc51431f067
zoccheQA <zocche.carolineqa@gmail.com>
2025-04-29 08:55:34 -0300

    Novo cenário

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://opensource-demo.orangehrmlive.com',
+    baseUrl: 'https://opensource-demo.orangehrmlive.com/web/index.php',
   },
 });
--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -8,11 +8,13 @@ describe('Orange HRM Tests', () => {
     loginButton: "[type='submit']",
     worngCredentialAlertInvalidCredentials: '.oxd-alert-content > .oxd-text',
     dasboardGrid: ".orangehrm-dashboard-grid",
-    WorngCredentialAlertRequiredPassword:'.oxd-input-group > .oxd-text'
+    WorngCredentialAlertRequiredPassword:'.oxd-input-group > .oxd-text',
+    myInfoButton: '[href="/web/index.php/pim/wiewMyDetailss"]'
+  
   }
  
   it('Login - Success', () => {
-    cy.visit('/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorslist.usernameField).type (userData.userSuccess.username)
     cy.get(selectorslist.passwordField).type (userData.userSuccess.password)
     cy.get(selectorslist.loginButton).click ()
@@ -20,27 +22,34 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorslist.dasboardGrid)
   })
   it('Login - Fail - Incorect User name', () => {
-    cy.visit('/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorslist.usernameField).type (userData.userFail.username)
     cy.get(selectorslist.passwordField).type (userData.userSuccess.password)
     cy.get(selectorslist.loginButton).click ()
     cy.get(selectorslist.worngCredentialAlertInvalidCredentials)
   })
   it('Login - Fail - Incorect Password', () => {
-    cy.visit('/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorslist.usernameField).type (userData.userSuccess.username)
     cy.get(selectorslist.passwordField).type (userData.userFail.password)
     cy.get(selectorslist.loginButton).click ()
     cy.get(selectorslist.worngCredentialAlertInvalidCredentials)
   })
   it('Login - Fail - Password not Added', () => {
-    cy.visit('/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorslist.usernameField).type (userData.userSuccess.username)
     cy.get(selectorslist.loginButton).click ()
     cy.get(selectorslist.WorngCredentialAlertRequiredPassword)
 })
 
-
-
+it('User info Update - Success', () => {
+  cy.visit('/auth/login')
+  cy.get(selectorslist.usernameField).type (userData.userSuccess.username)
+  cy.get(selectorslist.passwordField).type (userData.userSuccess.password)
+  cy.get(selectorslist.loginButton).click ()
+  cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+  cy.get(selectorslist.dasboardGrid)
+  cy.visit('/pim/viewMyDetails')
+  cy.get(".oxd-main-menu-item-wrapper [href='/web/index.php/pim/viewMyDetails']").click()
 })
-
+})
